e4df0540889a9746bebb16821246505ede107a58
Shuhao Zhang <tonyzhang19900609@gmail.com>
2020-07-05 18:04:58 +0200

    update lib name

--- cpp-lib/sesame/example_external/external_app.cpp
+++
@@ -1,6 +0,0 @@
-#include <join/join.h>
-
-int main(int argc, char *argv[]) {
-  foo_print_version();
-  return 0;
-}
--- cpp-lib/sesame/join/join.h
+++
@@ -1,8 +0,0 @@
-#ifndef sesame_H
-#define sesame_H
-
-#include <foo/version.h>
-
-void foo_print_version(void);
-
-#endif
