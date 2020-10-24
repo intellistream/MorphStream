c787225aa2812689eec98d5218109869ddc10874
Aqif Hamid <aqifhamid8@gmail.com>
2020-10-24 22:51:18 +0200

    Unified data holder to avoid re-reading events file in each thread.

---
+++ application/src/main/java/runners/TransactionsHolder.java
@@ -0,0 +1,9 @@
+package runners;
+
+import common.param.sl.TransactionEvent;
+
+import java.util.ArrayList;
+
+public class TransactionsHolder {
+    public static ArrayList<TransactionEvent> transactions = new ArrayList<>();
+}
