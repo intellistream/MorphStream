05e57388c483994b0d7fbc29072d6ef391646b65
Shuhao Zhang <tonyzhang19900609@gmail.com>
2021-06-07 13:53:07 +0800

    code refactor. Done for scheduler part.

--- state-engine/src/main/java/transaction/scheduler/nonlayered/NonLayeredScheduler.java
+++ state-engine/src/main/java/transaction/scheduler/nonlayered/NonLayeredScheduler.java
@@ -23,11 +23,11 @@ public class NonLayeredScheduler extends Scheduler {
 
         for (OperationChain oc : ocs) {
             if (!oc.hasParents() && oc.hasChildren())
-                context.OCwithChildren.add(oc);//operation chains with children
+                context.OCwithChildren.add(oc);//operation chains with children but with no parents.
             else if (!oc.hasParents())
                 context.IsolatedOC.add(oc);//isolated operation chains: no parents no children.
             else
-                oc.Listen(context.listener);//operation chains with parents listen to listener.
+                oc.Listen(context.listener);//operation chains with parents add to listener to be resolved.
         }
         context.totalSubmitted.addAndGet(ocs.size());
     }
@@ -45,18 +45,24 @@ public class NonLayeredScheduler extends Scheduler {
         return oc;
     }
 
-    public OperationChain Distribute(int threadId) {
+    @Override
+    protected OperationChain Distribute(int threadId) {
+
+        //1: First try to process oc with children but no parents.
         OperationChain oc = context.OCwithChildren.poll();
         if (oc == null && context.OCwithChildrenLocal[threadId].size() > 0) {
+            //2: Then try to process oc with children and parents are resolved (added by listener).
             oc = context.OCwithChildrenLocal[threadId].remove(context.OCwithChildrenLocal[threadId].size() - 1);
             if (context.OCwithChildrenLocal[threadId].size() > 0)
                 context.OCwithChildren.addAll(context.OCwithChildrenLocal[threadId]);
             context.OCwithChildrenLocal[threadId].clear();
         }
 
+        //3: If failed, try to process oc with no childeren and no parents.
         if (oc == null)
             oc = context.IsolatedOC.poll();
 
+        //4: If failed, try to process oc with no childeren and parents are resolved (added by listener).
         if (oc == null && context.IsolatedOCLocal[threadId].size() > 0) {
             oc = context.IsolatedOCLocal[threadId].remove(context.IsolatedOCLocal[threadId].size() - 1);
             if (context.IsolatedOCLocal[threadId].size() > 0)
