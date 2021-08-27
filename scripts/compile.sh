d3bad740246f62f7a05b781cd3fb38f3f2edee15
Mao Yancan <maoyancan@u.nus.edu>
2021-08-27 20:49:02 +0800

    add more scripts and todos

---
+++ scripts/compile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mvn clean package
+cp application/target/application-0.0.2-jar-with-dependencies.jar scripts/
--- scripts/runner.sh
+++ scripts/runner.sh
@@ -6,14 +6,14 @@ function ResetParameters() {
   events=983040
   fanoutDist="zipfcenter"
   idGenType="normal"
-  tt=24
+  tthread=24
   scheduler="BFS"
 }
 
 function runTStream() {
   java -Xms60g -Xmx60g -jar -d64 application-0.0.1-jar-with-dependencies.jar \
     --numberOfDLevels $numberOfDLevels \
-    -tt $tt \
+    --tthread $tthread \
     --totalEventsPerBatch $events \
     --numberOfBatches $numberOfBatches \
     --fanoutDist $fanoutDist  \
@@ -21,6 +21,7 @@ function runTStream() {
     --scheduler $scheduler
 }
 
+# run basic experiment for different algorithms
 function baselineEvaluation() {
   ResetParameters
   for scheduler in BFS DFS GS
@@ -31,5 +32,19 @@ function baselineEvaluation() {
           done
       done
 }
+
+# run basic experiment for different algorithms
+function withAbortEvaluation() {
+  ResetParameters
+  for scheduler in BFSA DFSA GSA
+      do
+          for events in 983040
+          do
+              runTStream
+          done
+      done
+}
+
+# TODO: more data generator properties are to be exposed
 
 baselineEvaluation
