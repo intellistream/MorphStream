d6f15b2b774814386979995d1999ced7316043b7
Mao Yancan <maoyancan@u.nus.edu>
2021-08-29 16:42:58 +0800

    code clean

---
+++ state-engine/src/main/java/scheduler/struct/layered/LayeredOperationChain.java
@@ -0,0 +1,49 @@
+package scheduler.struct.layered;
+
+import scheduler.struct.AbstractOperation;
+import scheduler.struct.OperationChain;
+
+import java.util.*;
+
+/**
+ * We still call it OperationChain in TPG but with different representation
+ * The OperationChain only tries to maintain a data structure for the ease of temporal dependencies construction.
+ */
+public abstract class LayeredOperationChain<ExecutionUnit extends AbstractOperation> extends OperationChain<ExecutionUnit> {
+
+    private boolean isDependencyLevelCalculated = false; // we only do this once before executing all OCs.
+    private int dependencyLevel = -1;
+
+    public LayeredOperationChain(String tableName, String primaryKey) {
+        super(tableName, primaryKey);
+    }
+
+    @Override
+    public Collection<LayeredOperationChain> getFDParents() {
+        return super.getFDParents();
+    }
+
+    public synchronized boolean hasValidDependencyLevel() {
+        return isDependencyLevelCalculated;
+    }
+
+    public int getDependencyLevel() {
+        return dependencyLevel;
+    }
+
+    public synchronized void updateDependencyLevel() {
+        if (isDependencyLevelCalculated)
+            return;
+        dependencyLevel = 0;
+        for (LayeredOperationChain<ExecutionUnit> parent : getFDParents()) {
+            if (!parent.hasValidDependencyLevel()) {
+                parent.updateDependencyLevel();
+            }
+
+            if (parent.getDependencyLevel() >= dependencyLevel) {
+                dependencyLevel = parent.getDependencyLevel() + 1;
+            }
+        }
+        isDependencyLevelCalculated = true;
+    }
+}
