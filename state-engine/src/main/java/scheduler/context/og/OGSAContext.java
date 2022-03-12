ffb7fa31298822f2efa53d2fa6122f1c4da08f64
Mao Yancan <maoyancan@u.nus.edu>
2022-03-12 21:22:22 +0800

    refactor data structures, operations are extending the same operation now.

--- state-engine/src/main/java/scheduler/context/og/OGSAContext.java
+++ state-engine/src/main/java/scheduler/context/og/OGSAContext.java
@@ -1,6 +1,6 @@
 package scheduler.context.og;
 
-public class OGDFSAContext extends OGDFSContext {
+public class OGDFSAContext extends OGSContext {
 
     public int rollbackLevel = -1; // initialized to 0 if thread not required to be rollbacked.
     public boolean isRollbacked = false; // initialized to 0 if thread not required to be rollbacked.
