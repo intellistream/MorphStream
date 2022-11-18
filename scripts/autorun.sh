59cf3d19b27ad75e60a4c8e57dcc30097818d0e1
Mao Yancan <maoyancan@u.nus.edu>
2022-11-18 22:03:15 +0800

    simply update scripts to be self-contained to run flink experiments

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-FLINK_DIR="/home/myc/build-target"
-FLINK_APP_DIR="/home/myc/workspace/MorphStream"
+PROJECT_DIR="/home/myc/workspace/myc/MorphStream"
+
+FLINK_DIR=${PROJECT_DIR}/flink-1.10
+REDIS_DIR=${PROJECT_DIR}/redis
+FLINK_APP_DIR=${PROJECT_DIR}
 
 # run flink clsuter
 function runFlink() {
-    echo "INFO: starting the cluster"
+    echo "INFO: starting the Flink cluster"
     if [[ -d ${FLINK_DIR}/log ]]; then
         rm -rf ${FLINK_DIR}/log
     fi
@@ -13,9 +16,13 @@ function runFlink() {
     ${FLINK_DIR}/bin/start-cluster.sh
 }
 
+function runRedis() {
+  echo "INFO: starting Redis Server"
+}
+
 # clean app specific related data
 function cleanEnv() {
-    mv ${FLINK_DIR}/log/*.out .
+    mv ${FLINK_DIR}/log/*.out ${job}.out
     rm -rf /tmp/flink*
     rm ${FLINK_DIR}/log/*
 }
@@ -32,25 +39,55 @@ function stopFlink() {
     cleanEnv
 }
 
+# clsoe flink clsuter
+function stopRedis() {
+    echo "INFO: stopping Redis Server"
+}
+    
+
 # initialization of the parameters
-init() {
+initSL() {
   # app level
   JAR="${FLINK_APP_DIR}/target/StreamLedger-1.0-SNAPSHOT.jar"
-  # job="StreamLedger.StreamLedger"
-  job="StreamLedger.StreamLedgerNoLock"
+  job="StreamLedger"
+}
+
+initSLNoLock() {
+  # app level
+  JAR="${FLINK_APP_DIR}/target/StreamLedger-1.0-SNAPSHOT.jar"
+  job="StreamLedgerNoLock"
 }
 
 # run applications
 function runApp() {
   echo "INFO: ${FLINK_DIR}/bin/flink run -c ${job} ${JAR} &"
-  ${FLINK_DIR}/bin/flink run -c ${job} ${JAR} &
+  ${FLINK_DIR}/bin/flink run -c StreamLedger.${job} ${JAR} &
+}
+
+build () {
+  mvn clean package
 }
 
 
-init
+# Run SL Exps
+initSL
 runFlink
+runRedis
+
 runApp
 
 python3 -c 'import time; time.sleep('"200"')'
 
+stopRedis
+stopFlink
+
+# Run SLNoLock Exps
+initSL
+runFlink
+
+runApp
+
+python3 -c 'import time; time.sleep('"200"')'
+
+stopRedis
 stopFlink
