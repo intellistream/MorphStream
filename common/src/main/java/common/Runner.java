303d3ce90eae7294bb0a5be726ef15e998b17547
curryzjj <curry_zhao@hust.edu.cn>
2023-07-03 23:18:39 +0800

    Modify dependencies identify

--- common/src/main/java/common/Runner.java
+++ common/src/main/java/common/Runner.java
@@ -179,7 +179,7 @@ public abstract class Runner implements IRunner {
 
     @Parameter(names = {"--window_size"}, description = "Window Size for the window operations.")
     public Integer windowSize = 1024;
-    @Parameter(names = {"--nondeterministic_ratio"}, description = "Ratio_of_Non_Deterministic_State_Access.(10 -> 0.1%)")
+    @Parameter(names = {"--nondeterministic_ratio"}, description = "Ratio_of_Non_Deterministic_State_Access.(10 -> 0.01%)")
     public Integer Ratio_of_Non_Deterministic_State_Access = 0;
 
     @Parameter(names = {"--txn_length"}, description = "Transaction Length.")
