26e16da8a258629d9e97660820ae20a2b3c1a9bf
Siqi <siqxiang@gmail.com>
2023-07-06 19:13:38 +0800

    Setup theme and frame layout

--- web-ui/src/app/app.component.ts
+++ web-ui/src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 }
---
+++ web-ui/src/theme/theme.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Theme, light, dark } from "./theme";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ThemeService {
+  private active: Theme = light;
+  private availableThemes: Theme[] = [light, dark];
+
+  getAvailableThemes(): Theme[] {
+    return this.availableThemes;
+  }
+
+  getActiveTheme(): Theme {
+    return this.active;
+  }
+
+  isDarkTheme(): boolean {
+    return this.active.name === dark.name;
+  }
+
+  setDarkTheme(): void {
+    this.setActiveTheme(dark);
+  }
+
+  setLightTheme(): void {
+    this.setActiveTheme(light);
+  }
+
+  setActiveTheme(theme: Theme): void {
+    this.active = theme;
+
+    Object.keys(this.active.properties).forEach(property => {
+      document.documentElement.style.setProperty(
+        property,
+        this.active.properties[property]
+      );
+    });
+  }
+}
