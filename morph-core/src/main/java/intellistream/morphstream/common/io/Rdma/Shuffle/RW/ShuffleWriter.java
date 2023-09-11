43954154e5c4c9196e6e1becea323735a3b09138
curryzjj <curry_zhao@hust.edu.cn>
2023-09-11 16:43:10 +0800

    Add RdmaWriter

--- morph-core/src/main/java/intellistream/morphstream/common/io/Rdma/Shuffle/RW/ShuffleWriter.java
+++ morph-core/src/main/java/intellistream/morphstream/common/io/Rdma/Shuffle/RW/ShuffleWriter.java
@@ -5,5 +5,6 @@ import javafx.util.Pair;
 import java.util.Iterator;
 
 public interface ShuffleWriter<K,V,C> {
-    public void write(Iterator<Pair<K,V>> records);
+    void write(Iterator<Pair<K,V>> records);
+    void stop(boolean success);
 }
