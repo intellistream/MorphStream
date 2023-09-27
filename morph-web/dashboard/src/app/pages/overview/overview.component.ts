9425f4fa47d5e81ad482885540261dc14d71dd11
Siqi <siqxiang@gmail.com>
2023-09-27 16:37:16 +0800

    Revamp morph-web overview page

--- morph-web/dashboard/src/app/pages/overview/overview.component.ts
+++ morph-web/dashboard/src/app/pages/overview/overview.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {OverviewService} from "./overview.service";
 import {BasicApplication} from "../../model/BasicApplication";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,8 @@ import {BasicApplication} from "../../model/BasicApplication";
 export class OverviewComponent implements OnInit {
   @ViewChild('runningAppContainer')
   scrollContainer!: ElementRef<HTMLElement>;
+
+  filterForm: FormGroup;
 
   // Data for testing
   // runningApplications: Application22[] = [
@@ -204,14 +207,71 @@ export class OverviewComponent implements OnInit {
   //   }
   // ];
 
-  constructor(private overviewService: OverviewService) {}
+  constructor(private overviewService: OverviewService, private formBuilder: FormBuilder) {}
 
-  completedApplications: BasicApplication[];
+  completedApplications: BasicApplication[] = [];
+
+  filteredJobs: BasicApplication[] = [];
 
   ngOnInit() {
+    this.tableIsLoading = true;
+
     this.overviewService.getAllHistoricalJobs().subscribe(res => {
-      console.log(res)
       this.completedApplications = res;
+      this.filteredJobs = this.completedApplications;
+      this.filterJob();
+    });
+
+    this.filterForm = this.formBuilder.group({
+      name: [null, null],
+      status: [null, null]
     })
+  }
+
+  tableIsLoading = false;
+  currentPageIndex = 1;
+  pageSize = 10;
+  currentPageJob: BasicApplication[] = [];
+
+  onPageIndexChange(index: number) {
+    this.currentPageIndex = index;
+    this.filterJob();
+  }
+
+  onPageSizeChange(size: number) {
+    this.pageSize = size;
+    this.filterJob()
+  }
+
+
+  onFilterJob() {
+    this.tableIsLoading = true;
+    this.currentPageIndex = 1;
+    this.filterJob();
+  }
+
+  filterJob() {
+    const filterName = this.filterForm.value.name;
+    const filterStatus = this.filterForm.value.status;
+    this.filteredJobs = this.completedApplications;
+
+    if (filterName != null) {
+      this.filteredJobs = this.filteredJobs.filter(job =>
+        job.name.includes(`${filterName}`)
+      );
+    }
+
+    if (filterStatus != null) {
+      this.filteredJobs = this.filteredJobs.filter(job =>
+        job.isRunning == filterStatus
+      );
+    }
+
+    const startIndex = (this.currentPageIndex-1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.currentPageJob = this.filteredJobs.slice(startIndex, endIndex);
+    this.tableIsLoading = false;
+
+    console.log(this.currentPageJob)
   }
 }
