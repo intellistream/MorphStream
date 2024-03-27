66e7f77e05cd4a411fde1b2f1de8bfd8312273cc
curryzjj <curry_zhao@hust.edu.cn>
2024-03-27 17:07:10 +0800

    add RemoteDB

--- scripts/FaaS/compile.sh
+++ scripts/FaaS/compile.sh
@@ -4,4 +4,7 @@ javac -cp .:$RUNDIR/morph-core/src/main/java -h $RUNDIR/morph-lib/include $RUNDI
 cd $LIBDIR
 gcc -fPIC -shared -I$JAVA_HOME/include -I$RUNDIR/morph-lib/include $RUNDIR/morph-lib/src/RemoteCallLibrary.c -o RemoteCallLibrary.so
 cd -
+cd ../
+mvn install -DskipTests
+cd -
 
