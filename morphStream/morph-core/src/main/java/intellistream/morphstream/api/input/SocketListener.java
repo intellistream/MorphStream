18f66c89d432d798a206fa22bc611fb14b73ac0f
zhonghao-yang <yzh98640860@gmail.com>
2024-05-07 21:00:19 +0800

    Updated socket listener message passing

--- morphStream/morph-core/src/main/java/intellistream/morphstream/api/input/SocketListener.java
+++ morphStream/morph-core/src/main/java/intellistream/morphstream/api/input/SocketListener.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
@@ -76,126 +77,204 @@ public class SocketListener implements Runnable { //A single thread that listens
             this.instanceID = instanceID;
         }
 
-        @Override
         public void run() {
             try (InputStream input = clientSocket.getInputStream()) {
-                // Buffer to hold the read data
-                List<Byte> messageBuffer = new ArrayList<>();
-                int readByte;
+                ByteBuffer byteBuffer = ByteBuffer.allocate(8); // 4 bytes for request type, 4 bytes for numElements
 
-                while (true) {  // Changed from while ((readByte = input.read()) != -1)
-                    try {
-                        if ((readByte = input.read()) != -1) {
-                            if (readByte == msgSeparator) {
-                                byte[] message = new byte[messageBuffer.size()];
-                                for (int i = 0; i < message.length; i++) {
-                                    message[i] = messageBuffer.get(i);
-                                }
-//                                System.out.println("Received from " + clientSocket.getRemoteSocketAddress() + ": " + new String(message));
+                while (true) {
+                    byteBuffer.clear();
+                    int bytesRead = 0;
 
-                                if (message.length == 1) {
-                                    System.out.println("Error: reading invalid message: " + message[0]);
-                                    System.out.println("Total requests received: " + requestCounter);
-                                } else {
-                                    List<byte[]> splitByteArrays = splitByteArray(message, fullSeparator);
-                                    int target = decodeInt(splitByteArrays.get(0), 0);
-
-                                    if (target == 0) {
-                                        monitorQueue.add(byteToPatternData(instanceID, splitByteArrays));
-                                    } else if (target == 1) {
-                                        partitionQueue.add(byteToPartitionData(instanceID, splitByteArrays));
-                                    } else if (target == 2) {
-                                        cacheQueue.add(byteToCacheData(instanceID, splitByteArrays));
-                                    } else if (target == 3) {
-                                        offloadQueue.add(byteToOffloadData(instanceID, splitByteArrays));
-                                    } else if (target == 4) {
-                                        tpgQueues.get(rrIndex).add(byteToTPGData(instanceID, splitByteArrays));
-                                        rrIndex = (rrIndex + 1) % tpgThreadNum;
-                                    }
-                                    // Clear the buffer for the next message
-                                    messageBuffer.clear();
-                                    requestCounter++;
-                                }
-
-                            } else {
-                                messageBuffer.add((byte) readByte);
-                            }
-                        } else {
-                            // -1 returned from read indicates client has closed the connection properly
-                            System.out.println("Client has closed the connection: " + clientSocket.getRemoteSocketAddress());
-                            System.out.println("Total request received: " + requestCounter);
-                            break;
+                    // Read 8 bytes for request type and numElements
+                    while (bytesRead < 8) {
+                        int byteValue = input.read();
+                        if (byteValue == -1) {
+                            System.out.println("Unexpected end of stream while reading request type and numElements.");
+                            return;
                         }
-                    } catch (SocketException se) {
-                        // Specific handling for socket related exceptions
-                        System.out.println("Socket exception (likely connection reset by client): " + se.getMessage());
-                        System.out.println("Total request received: " + requestCounter);
-                        break;
-                    } catch (IOException ie) {
-                        // General I/O exceptions
-                        System.out.println("IOException during communication: " + ie.getMessage());
-                        System.out.println("Total request received: " + requestCounter);
-                        break;
+                        byteBuffer.put((byte) byteValue);
+                        bytesRead++;
+                    }
+
+                    byteBuffer.rewind(); // Reset position for reading
+                    int target = byteBuffer.getInt();
+                    int numElements = byteBuffer.getInt();
+                    byte[] message = new byte[numElements];
+                    bytesRead = 0;
+
+                    // Read numElements bytes for the message
+                    while (bytesRead < numElements) {
+                        int byteValue = input.read();
+                        if (byteValue == -1) {
+                            System.out.println("Unexpected end of stream while reading message bytes.");
+                            return;
+                        }
+                        message[bytesRead] = (byte) byteValue;
+                        bytesRead++;
+                    }
+
+                    // Process the message based on the request type
+                    switch (target) {
+                        case 0:
+                            monitorQueue.add(byteToPatternData(instanceID, message));
+                            break;
+                        case 1:
+                            partitionQueue.add(byteToPartitionData(instanceID, message));
+                            break;
+                        case 2:
+                            cacheQueue.add(byteToCacheData(instanceID, message));
+                            break;
+                        case 3:
+                            offloadQueue.add(byteToOffloadData(instanceID, message));
+                            break;
+                        case 4:
+                            tpgQueues.get(rrIndex).add(byteToTPGData(instanceID, message));
+                            rrIndex = (rrIndex + 1) % tpgThreadNum;
+                            break;
+                        default:
+                            System.out.println("Unknown request type: " + target);
                     }
                 }
-            } catch (Exception e) {
-                System.out.println("Exception when setting up input stream: " + e.getMessage());
-                System.out.println("Exception type: " + e.getClass().getName());
-                e.printStackTrace();
-                System.out.println("Total request received: " + requestCounter);
-            }
-            finally {
+            } catch (IOException e) {
+                System.out.println("IOException during communication: " + e.getMessage());
+            } finally {
                 try {
                     clientSocket.close();
                     System.out.println("Closed client socket.");
-                    System.out.println("Total request received: " + requestCounter);
                 } catch (IOException e) {
                     System.out.println("Error closing client socket: " + e.getMessage());
-                    System.out.println("Total request received: " + requestCounter);
                 }
             }
         }
+
+
+
+//        @Override
+//        public void run() {
+//            try (InputStream input = clientSocket.getInputStream()) {
+//                // Buffer to hold the read data
+//                List<Byte> messageBuffer = new ArrayList<>();
+//                int readByte;
+//
+//                while (true) {  // Changed from while ((readByte = input.read()) != -1)
+//                    try {
+//                        if ((readByte = input.read()) != -1) {
+//                            if (readByte == msgSeparator) {
+//                                byte[] message = new byte[messageBuffer.size()];
+//                                for (int i = 0; i < message.length; i++) {
+//                                    message[i] = messageBuffer.get(i);
+//                                }
+////                                System.out.println("Received from " + clientSocket.getRemoteSocketAddress() + ": " + new String(message));
+//
+//                                if (message.length == 1) {
+//                                    System.out.println("Error: reading invalid message: " + message[0]);
+//                                    System.out.println("Total requests received: " + requestCounter);
+//                                } else {
+//                                    List<byte[]> splitByteArrays = splitByteArray(message, fullSeparator);
+//                                    int target = decodeInt(splitByteArrays.get(0), 0);
+//
+//                                    if (target == 0) {
+//                                        monitorQueue.add(byteToPatternData(instanceID, splitByteArrays));
+//                                    } else if (target == 1) {
+//                                        partitionQueue.add(byteToPartitionData(instanceID, splitByteArrays));
+//                                    } else if (target == 2) {
+//                                        cacheQueue.add(byteToCacheData(instanceID, splitByteArrays));
+//                                    } else if (target == 3) {
+//                                        offloadQueue.add(byteToOffloadData(instanceID, splitByteArrays));
+//                                    } else if (target == 4) {
+//                                        tpgQueues.get(rrIndex).add(byteToTPGData(instanceID, splitByteArrays));
+//                                        rrIndex = (rrIndex + 1) % tpgThreadNum;
+//                                    }
+//                                    // Clear the buffer for the next message
+//                                    messageBuffer.clear();
+//                                    requestCounter++;
+//                                }
+//
+//                            } else {
+//                                messageBuffer.add((byte) readByte);
+//                            }
+//                        } else {
+//                            // -1 returned from read indicates client has closed the connection properly
+//                            System.out.println("Client has closed the connection: " + clientSocket.getRemoteSocketAddress());
+//                            System.out.println("Total request received: " + requestCounter);
+//                            break;
+//                        }
+//                    } catch (SocketException se) {
+//                        // Specific handling for socket related exceptions
+//                        System.out.println("Socket exception (likely connection reset by client): " + se.getMessage());
+//                        System.out.println("Total request received: " + requestCounter);
+//                        break;
+//                    } catch (IOException ie) {
+//                        // General I/O exceptions
+//                        System.out.println("IOException during communication: " + ie.getMessage());
+//                        System.out.println("Total request received: " + requestCounter);
+//                        break;
+//                    }
+//                }
+//            } catch (Exception e) {
+//                System.out.println("Exception when setting up input stream: " + e.getMessage());
+//                System.out.println("Exception type: " + e.getClass().getName());
+//                e.printStackTrace();
+//                System.out.println("Total request received: " + requestCounter);
+//            }
+//            finally {
+//                try {
+//                    clientSocket.close();
+//                    System.out.println("Closed client socket.");
+//                    System.out.println("Total request received: " + requestCounter);
+//                } catch (IOException e) {
+//                    System.out.println("Error closing client socket: " + e.getMessage());
+//                    System.out.println("Total request received: " + requestCounter);
+//                }
+//            }
+//        }
     }
 
 
-    private static PatternData byteToPatternData(int instanceID, List<byte[]> splitByteArrays) {
-        int tupleID = decodeInt(splitByteArrays.get(1), 0);
-        boolean value = decodeBoolean(splitByteArrays.get(2), 0);
-        return new PatternData(0, instanceID, tupleID, value);
+    private static PatternData byteToPatternData(int instanceID, byte[] messageBytes) {
+        int tupleID = decodeInt(copySubarray(messageBytes, 1, 4), 0);
+        boolean isWrite = decodeBoolean(copySubarray(messageBytes, 6, 6), 0);
+        return new PatternData(0, instanceID, tupleID, isWrite);
     }
 
-    private static PartitionData byteToPartitionData(int instanceID, List<byte[]> splitByteArrays) {
-        int tupleID = decodeInt(splitByteArrays.get(1), 0);
-        int value = decodeInt(splitByteArrays.get(2), 0);
-        return new PartitionData(-1,-1,instanceID, tupleID, value);
+    private static PartitionData byteToPartitionData(int instanceID, byte[] messageBytes) {
+        int tupleID = decodeInt(copySubarray(messageBytes, 1, 4), 0);
+        int value = decodeInt(copySubarray(messageBytes, 6, 9), 0);
+        return new PartitionData(-1, -1, instanceID, tupleID, value);
     }
 
-    private static CacheData byteToCacheData(int instanceID, List<byte[]> splitByteArrays) {
-        int tupleID = decodeInt(splitByteArrays.get(1), 0);
-        int value = decodeInt(splitByteArrays.get(2), 0);
+    private static CacheData byteToCacheData(int instanceID, byte[] messageBytes) {
+        int tupleID = decodeInt(copySubarray(messageBytes, 1, 4), 0);
+        int value = decodeInt(copySubarray(messageBytes, 6, 9), 0);
         return new CacheData(0, instanceID, tupleID, value);
     }
 
-    private static OffloadData byteToOffloadData(int instanceID, List<byte[]> splitByteArrays) {
-        long timestamp = decodeLong(splitByteArrays.get(1), 0);
-        long txnReqID = decodeLong(splitByteArrays.get(2), 0);
-        int tupleID = decodeInt(splitByteArrays.get(3), 0);
-        int txnIndex = decodeInt(splitByteArrays.get(4), 0);
-        int saIndex = decodeInt(splitByteArrays.get(5), 0);
-        int isAbort = decodeInt(splitByteArrays.get(6), 0);
-        return new OffloadData(timestamp, instanceID, txnReqID, tupleID, txnIndex, saIndex, isAbort, -1);
+    private static OffloadData byteToOffloadData(int instanceID, byte[] messageBytes) { //TODO: Align with libVNF
+        long timestamp = decodeLong(copySubarray(messageBytes, 0, 7), 0);
+        long txnReqID = decodeLong(copySubarray(messageBytes, 8, 15), 0);
+        int tupleID = decodeInt(copySubarray(messageBytes, 0, 3), 0);
+        int txnIndex = decodeInt(copySubarray(messageBytes, 4, 7), 0);
+        int saIndex = decodeInt(copySubarray(messageBytes, 8, 11), 0);
+        int isAbort = decodeInt(copySubarray(messageBytes, 12, 15), 0);
+        return new OffloadData(-1, instanceID, 0, tupleID, txnIndex, saIndex, isAbort, -1);
     }
 
-    private static TransactionalEvent byteToTPGData(int instanceID, List<byte[]> splitByteArrays) {
-        long timestamp = decodeLong(splitByteArrays.get(1), 0);
-        long txnReqID = decodeLong(splitByteArrays.get(2), 0);
-        int tupleID = decodeInt(splitByteArrays.get(3), 0);
-        int txnIndex = decodeInt(splitByteArrays.get(4), 0);
-        int saIndex = decodeInt(splitByteArrays.get(5), 0);
-        int isAbort = decodeInt(splitByteArrays.get(6), 0);
+    private static TransactionalEvent byteToTPGData(int instanceID, byte[] messageBytes) {
+        long timestamp = decodeLong(copySubarray(messageBytes, 0, 7), 0);
+        long txnReqID = decodeLong(copySubarray(messageBytes, 8, 15), 0);
+        int tupleID = decodeInt(copySubarray(messageBytes, 0, 3), 0);
+        int txnIndex = decodeInt(copySubarray(messageBytes, 4, 7), 0);
+        int saIndex = decodeInt(copySubarray(messageBytes, 8, 11), 0);
+        int isAbort = decodeInt(copySubarray(messageBytes, 12, 15), 0);
         return new TransactionalVNFEvent(-1, instanceID, timestamp, txnReqID, tupleID, txnIndex, saIndex, isAbort);
     }
 
+    public static byte[] copySubarray(byte[] inputArray, int startIndex, int endIndex) {
+        int subarrayLength = endIndex - startIndex + 1;
+        byte[] subarray = new byte[subarrayLength];
+        System.arraycopy(inputArray, startIndex, subarray, 0, subarrayLength);
+        return subarray;
+    }
 
     private static long decodeLong(byte[] bytes, int offset) {
         long value = 0;
