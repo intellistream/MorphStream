fbddc40a5ee73a8fb880285e3aac2420679a8ce8
curryzjj <curry_zhao@hust.edu.cn>
2024-05-23 15:41:37 +0800

    Add Application

---
+++ scripts/FaaS/SocialNetwork/driver.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+source ../../dir.sh || exit
+DAGName=$1
+function ResetParameters() {
+    #Cluster Configurations
+    isDriver=1
+    workerId=0
+    workerNum=2
+    tthread=10
+    clientNum=10
+    frontendNum=20
+    clientClassName="client.$DAGName"
+    #Network Configurations
+    isRDMA=1
+    driverHost="10.10.10.3"
+    driverPort=5570
+    workerHosts="10.10.10.1,10.10.10.2"
+    workerPorts="5550,5540"
+    CircularBufferCapacity=`expr 1024 \* 1024 \* 1024`
+    TableBufferCapacity=`expr 1024 \* 1024 \* 1024`
+    CacheBufferCapacity=`expr 1024 \* 1024 \* 1024`
+    RemoteOperationBufferCapacity=`expr 1024 \* 1024 \* 1024`
+    sendMessagePerFrontend=`expr 50 \* $tthread \* $workerNum / $frontendNum`
+    totalBatch=4
+    returnResultPerExecutor=`expr 50 \* $frontendNum / $workerNum / $tthread`
+    shuffleType=3
+    #Database Configurations
+    isRemoteDB=1
+    numberItemsForTables="10000;10000;100000"
+    NUM_ITEMS=100000
+    tableNames="user_pwd;user_profile;tweet"
+    keyDataTypesForTables="String;String;String"
+    valueDataTypesForTables="String;String;String"
+    valueDataSizesForTables="64;128;128"
+    valueNamesForTables="pwd;profile;tweet"
+    #Input Configurations
+    rootFilePath="${RSTDIR}"
+    inputFileType=0
+    eventTypes="userLogin;userProfile;getTimeLine;postTweet"
+    tableNameForEvents="user_pwd;user_profile;tweet;tweet"
+    keyNumberForEvents="1;1;1;1"
+    valueNameForEvents="password;;;tweet"
+    valueSizeForEvents="64;0;0;128"
+    eventRatio="15;30;50;5"
+    ratioOfMultiPartitionTransactionsForEvents="0;0;0;0"
+    stateAccessSkewnessForEvents="0;0;0;0"
+    abortRatioForEvents="0;0;0;0"
+    isCyclic=0
+    isDynamic=0
+    workloadType="default,unchanging,unchanging,unchanging"
+    shiftRate=1
+    checkpointInterval=`expr $sendMessagePerFrontend \* $frontendNum \* $totalBatch`
+    totalEvents=`expr $checkpointInterval \* $shiftRate \* 1`
+    #System Configurations
+    schedulerPool="DScheduler"
+    scheduler="DScheduler"
+    defaultScheduler="DScheduler"
+    CCOption=3 #TSTREAM
+    complexity=0
+}
+
+function runApplication() {
+  echo "-Xms60g -Xmx60g -Xss100M -XX:+PrintGCDetails -Xmn40g -XX:+UseG1GC -jar -d64 ${JAR} -Djava.library.path=${LIBDIR} \
+      --isDriver $isDriver \
+      --workerId $workerId \
+      --workerNum $workerNum \
+      --tthread $tthread \
+      --clientNum $clientNum \
+      --frontendNum $frontendNum \
+      --clientClassName $clientClassName \
+      --isRDMA $isRDMA \
+      --driverHost $driverHost \
+      --driverPort $driverPort \
+      --workerHosts $workerHosts \
+      --workerPorts $workerPorts \
+      --CircularBufferCapacity $CircularBufferCapacity \
+      --TableBufferCapacity $TableBufferCapacity \
+      --CacheBufferCapacity $CacheBufferCapacity \
+      --RemoteOperationBufferCapacity $RemoteOperationBufferCapacity \
+      --sendMessagePerFrontend $sendMessagePerFrontend \
+      --returnResultPerExecutor $returnResultPerExecutor \
+      --totalBatch $totalBatch \
+      --shuffleType $shuffleType \
+      --isRemoteDB $isRemoteDB \
+      --numberItemsForTables $numberItemsForTables \
+      --NUM_ITEMS $NUM_ITEMS \
+      --tableNames $tableNames \
+      --keyDataTypesForTables $keyDataTypesForTables \
+      --valueDataTypesForTables $valueDataTypesForTables \
+      --valueNamesForTables $valueNamesForTables \
+      --valueDataSizesForTables $valueDataSizesForTables \
+      --rootFilePath $rootFilePath \
+      --inputFileType $inputFileType \
+      --eventTypes $eventTypes \
+      --tableNameForEvents $tableNameForEvents \
+      --keyNumberForEvents $keyNumberForEvents \
+      --valueNameForEvents $valueNameForEvents \
+      --valueSizeForEvents $valueSizeForEvents \
+      --eventRatio $eventRatio \
+      --ratioOfMultiPartitionTransactionsForEvents $ratioOfMultiPartitionTransactionsForEvents \
+      --stateAccessSkewnessForEvents $stateAccessSkewnessForEvents \
+      --abortRatioForEvents $abortRatioForEvents \
+      --isCyclic $isCyclic \
+      --isDynamic $isDynamic \
+      --workloadType $workloadType \
+      --shiftRate $shiftRate \
+      --totalEvents $totalEvents \
+      --schedulerPool $schedulerPool \
+      --checkpoint_interval $checkpointInterval \
+      --scheduler $scheduler \
+      --defaultScheduler $defaultScheduler \
+      --CCOption $CCOption \
+      --complexity $complexity \
+            "
+  java -Xms100g -Xmx100g -Xss100M -XX:+PrintGCDetails -Xmn80g -XX:+UseG1GC -Djava.library.path=$LIBDIR -jar -d64 $JAR \
+      --isDriver $isDriver \
+      --workerId $workerId \
+      --workerNum $workerNum \
+      --tthread $tthread \
+      --clientNum $clientNum \
+      --frontendNum $frontendNum \
+      --clientClassName $clientClassName \
+      --isRDMA $isRDMA \
+      --driverHost $driverHost \
+      --driverPort $driverPort \
+      --workerHosts $workerHosts \
+      --workerPorts $workerPorts \
+      --CircularBufferCapacity $CircularBufferCapacity \
+      --TableBufferCapacity $TableBufferCapacity \
+      --CacheBufferCapacity $CacheBufferCapacity \
+      --RemoteOperationBufferCapacity $RemoteOperationBufferCapacity \
+      --sendMessagePerFrontend $sendMessagePerFrontend \
+      --returnResultPerExecutor $returnResultPerExecutor \
+      --totalBatch $totalBatch \
+      --shuffleType $shuffleType \
+      --isRemoteDB $isRemoteDB \
+      --numberItemsForTables $numberItemsForTables \
+      --NUM_ITEMS $NUM_ITEMS \
+      --tableNames $tableNames \
+      --keyDataTypesForTables $keyDataTypesForTables \
+      --valueDataTypesForTables $valueDataTypesForTables \
+      --valueNamesForTables $valueNamesForTables \
+      --valueDataSizesForTables $valueDataSizesForTables \
+      --rootFilePath $rootFilePath \
+      --inputFileType $inputFileType \
+      --eventTypes $eventTypes \
+      --tableNameForEvents $tableNameForEvents \
+      --keyNumberForEvents $keyNumberForEvents \
+      --valueNameForEvents $valueNameForEvents \
+      --valueSizeForEvents $valueSizeForEvents \
+      --eventRatio $eventRatio \
+      --ratioOfMultiPartitionTransactionsForEvents $ratioOfMultiPartitionTransactionsForEvents \
+      --stateAccessSkewnessForEvents $stateAccessSkewnessForEvents \
+      --abortRatioForEvents $abortRatioForEvents \
+      --isCyclic $isCyclic \
+      --isDynamic $isDynamic \
+      --workloadType $workloadType \
+      --shiftRate $shiftRate \
+      --totalEvents $totalEvents \
+      --schedulerPool $schedulerPool \
+      --checkpoint_interval $checkpointInterval \
+      --scheduler $scheduler \
+      --defaultScheduler $defaultScheduler \
+      --CCOption $CCOption \
+      --complexity $complexity
+}
+
+function application_runner() {
+ ResetParameters
+ runApplication
+}
+application_runner
+rm -rf ${RSTDIR}/inputs/client.$DAGName
