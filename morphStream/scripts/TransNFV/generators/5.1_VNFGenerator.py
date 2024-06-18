0227063278b0b3e70013399f14509db4df035c9e
zhonghao-yang <yzh98640860@gmail.com>
2024-06-18 20:54:23 +0800

    Add 10 VNF workload

--- morphStream/scripts/TransNFV/generators/5.1_VNFGenerator.py
+++ morphStream/scripts/TransNFV/generators/5.1_VNFGenerator.py
@@ -4,14 +4,12 @@ import os
 import shutil
 
 class PatternGenerator:
-    def __init__(self, tuple_range=5000, instance_count=4, request_count=100000, pattern_1_prob=0.8, pattern_2_prob=1, pattern_3_prob=0.8):
+    def __init__(self, tuple_range=5000, instance_count=4, request_count=100000, pattern_prob=0.8):
         self.tuple_range = tuple_range
         self.instance_count = instance_count
         self.request_count = request_count
         self.base_dir = os.path.join(os.getcwd(), 'scripts/TransNFV/pattern_files/5.1')
-        self.type_zero_probability = pattern_1_prob  # Probability for type 0 in sharedReaders pattern
-        self.type_one_probability = pattern_2_prob    # Probability for type 1 in sharedWriters pattern
-        self.type_two_probability = pattern_3_prob    # Probability for type 2 in mutualInteractive pattern
+        self.pattern_probability = pattern_prob  # Probability for type 0 in sharedReaders pattern
 
     def generate_files(self):
         # Remove existing files and directories before creating new ones
@@ -19,22 +17,28 @@ class PatternGenerator:
             shutil.rmtree(self.base_dir)
         os.makedirs(self.base_dir)
 
-
         # Define the patterns
-        patterns = {
-            'loneOperative': self.generate_lone_operative,
-            'sharedReaders': self.generate_shared_readers,
-            'sharedWriters': self.generate_shared_writers,
-            'mutualInteractive': self.generate_mutual_interactive  # Placeholder for another pattern
+        vnfs = {
+            '1_firewall': self.generate_firewall,
+            '2_nat': self.generate_nat,
+            '3_lb': self.generate_lb,
+            '4_trojan_detector': self.generate_trojan_detector,
+            '5_portscan_detector': self.generate_portscan_detector,
+            '6_prads': self.generate_prads,
+            '7_session_border_controller': self.generate_session_border_controller,
+            '8_ips': self.generate_ips,
+            '9_squid': self.generate_squid,
+            '10_adaptive_traffic_shaper': self.generate_adaptive_traffic_shaper
         }
 
-        for pattern_name, generation_method in patterns.items():
-            pattern_dir = os.path.join(self.base_dir, f"instanceNum_{self.instance_count}", pattern_name)
+        for vnf_name, generation_method in vnfs.items():
+            pattern_dir = os.path.join(self.base_dir, f"instanceNum_{self.instance_count}", vnf_name)
             if not os.path.exists(pattern_dir):
                 os.makedirs(pattern_dir)
             generation_method(pattern_dir)
 
-    def generate_lone_operative(self, pattern_dir):
+
+    def generate_firewall(self, pattern_dir):
         partition_size = self.tuple_range // self.instance_count
         for instance_index in range(self.instance_count):
             min_range = instance_index * partition_size
@@ -48,50 +52,129 @@ class PatternGenerator:
                     writer.writerow([request_id, tuple_id, type_id])
             print(f'Generated file: {file_name}')
 
-    def generate_shared_readers(self, pattern_dir):
+    def generate_nat(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
         for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
             file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
             with open(file_name, mode='w', newline='') as file:
                 writer = csv.writer(file)
                 for request_id in range(self.request_count):
-                    tuple_id = random.randint(0, self.tuple_range - 1)
-                    # Generate type based on the class variable probability for type 0 (R)
-                    type_id = 0 if random.random() < self.type_zero_probability else random.randint(1, 2)
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 0 if random.random() < self.pattern_probability else 2
                     writer.writerow([request_id, tuple_id, type_id])
             print(f'Generated file: {file_name}')
 
-    def generate_shared_writers(self, pattern_dir):
+    def generate_lb(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
         for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
             file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
             with open(file_name, mode='w', newline='') as file:
                 writer = csv.writer(file)
                 for request_id in range(self.request_count):
-                    tuple_id = random.randint(0, self.tuple_range - 1)
-                    # Generate type based on the class variable probability for type 1 (W)
-                    type_id = 1 if random.random() < self.type_one_probability else random.choice([0, 2])
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 0 if random.random() < self.pattern_probability else 2
                     writer.writerow([request_id, tuple_id, type_id])
             print(f'Generated file: {file_name}')
 
-    def generate_mutual_interactive(self, pattern_dir):
+    def generate_trojan_detector(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
         for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
             file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
             with open(file_name, mode='w', newline='') as file:
                 writer = csv.writer(file)
                 for request_id in range(self.request_count):
-                    tuple_id = random.randint(0, self.tuple_range - 1)
-                    # Generate type based on the class variable probability for type 2 (R&W)
-                    type_id = 2 if random.random() < self.type_two_probability else random.choice([0, 1])
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 2
                     writer.writerow([request_id, tuple_id, type_id])
             print(f'Generated file: {file_name}')
 
-    def generate_random(self, pattern_dir):
+    def generate_portscan_detector(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
         for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
             file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
             with open(file_name, mode='w', newline='') as file:
                 writer = csv.writer(file)
                 for request_id in range(self.request_count):
-                    tuple_id = random.randint(0, self.tuple_range - 1)
-                    type_id = random.randint(0, 2)
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 2
+                    writer.writerow([request_id, tuple_id, type_id])
+            print(f'Generated file: {file_name}')
+
+    def generate_prads(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
+        for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
+            file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
+            with open(file_name, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                for request_id in range(self.request_count):
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 1 if random.random() < self.pattern_probability else 0
+                    writer.writerow([request_id, tuple_id, type_id])
+            print(f'Generated file: {file_name}')
+
+    def generate_session_border_controller(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
+        for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
+            file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
+            with open(file_name, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                for request_id in range(self.request_count):
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 0 if random.random() < self.pattern_probability else 1
+                    writer.writerow([request_id, tuple_id, type_id])
+            print(f'Generated file: {file_name}')
+
+    def generate_ips(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
+        for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
+            file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
+            with open(file_name, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                for request_id in range(self.request_count):
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 2 if random.random() < self.pattern_probability else 0
+                    writer.writerow([request_id, tuple_id, type_id])
+            print(f'Generated file: {file_name}')
+
+    def generate_squid(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
+        for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
+            file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
+            with open(file_name, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                for request_id in range(self.request_count):
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 2
+                    writer.writerow([request_id, tuple_id, type_id])
+            print(f'Generated file: {file_name}')
+
+    def generate_adaptive_traffic_shaper(self, pattern_dir):
+        partition_size = self.tuple_range // self.instance_count
+        for instance_index in range(self.instance_count):
+            min_range = instance_index * partition_size
+            max_range = (instance_index + 1) * partition_size - 1
+            file_name = os.path.join(pattern_dir, f'instance_{instance_index}.csv')
+            with open(file_name, mode='w', newline='') as file:
+                writer = csv.writer(file)
+                for request_id in range(self.request_count):
+                    tuple_id = random.randint(min_range, max_range)
+                    type_id = 0 if random.random() < self.pattern_probability else 1
                     writer.writerow([request_id, tuple_id, type_id])
             print(f'Generated file: {file_name}')
 
