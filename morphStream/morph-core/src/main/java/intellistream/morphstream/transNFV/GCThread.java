367fcb8a2c94b4006fae0590de152a00998ca20d
zhonghao-yang <yzh98640860@gmail.com>
2024-08-19 18:12:37 +0800

    Test git push

--- morphStream/morph-core/src/main/java/intellistream/morphstream/transNFV/GCThread.java
+++ morphStream/morph-core/src/main/java/intellistream/morphstream/transNFV/GCThread.java
@@ -4,11 +4,14 @@ import intellistream.morphstream.api.launcher.MorphStreamEnv;
 import intellistream.morphstream.engine.txn.db.DatabaseException;
 import intellistream.morphstream.engine.txn.storage.StorageManager;
 
+import java.util.HashMap;
+
 public class GCThread implements Runnable {
 
     private static final StorageManager storageManager = MorphStreamEnv.get().database().getStorageManager();
     private static final int gcInterval = MorphStreamEnv.get().configuration().getInt("gcInterval");
     private static final int NUM_ITEMS = MorphStreamEnv.get().configuration().getInt("NUM_ITEMS");
+    private static final HashMap<String, Integer> executorEndTimestamps = new HashMap<>();
     private int currentGCInterval = 0;
 
     private void garbageCollection(int tupleID, int timestamp) {
@@ -22,6 +25,7 @@ public class GCThread implements Runnable {
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
+            // Periodically fetch the latest operation timestamps from executors
             // TODO: Garbage identification: When all executors finish processing the current GC_batch
             // Garbage collection
             currentGCInterval += gcInterval;
