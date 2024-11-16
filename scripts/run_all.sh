59530413a8d08209e3528fea1a59d07a5a7b48ed
Mao Yancan <maoyancan@u.nus.edu>
2024-11-16 21:59:42 +0800

    Re-upload example experiment results, update run_all to clear cache before experiment

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -4,6 +4,10 @@
 project_Dir="/home/myc/workspace/myc/MorphStream_Reproduce"
 jar_Dir="${project_Dir}/application/target/application-0.0.2-jar-with-dependencies.jar"
 
+# Clear cached synthetic data colocated with /result/data
+
+rm -rf $project_Dir/result/data
+
 # Modify global.sh in the same folder
 echo "Updating global.sh with project_Dir and jar_Dir..."
 global_file="$project_Dir/scripts/global.sh" # Assuming it's in the same directory as run_all.sh
@@ -49,32 +53,32 @@ run_experiment() {
     echo "$experiment_name completed."
 }
 
-# Comparison with Conventional SPEs (Fig.10)
-run_experiment "$project_Dir/scripts/PerformanceEvaluation/PerformanceComparison" "PerformanceComparison.sh" "Performance Comparison with Conventional SPEs"
+# # Comparison with Conventional SPEs (Fig.10)
+# run_experiment "$project_Dir/scripts/PerformanceEvaluation/PerformanceComparison" "PerformanceComparison.sh" "Performance Comparison with Conventional SPEs"
 
-# Dynamic Workloads Evaluation (Fig.11)
-run_experiment "$project_Dir/scripts/PerformanceEvaluation" "DynamicWorkload.sh" "Dynamic Workload Evaluation"
+# # Dynamic Workloads Evaluation (Fig.11)
+# run_experiment "$project_Dir/scripts/PerformanceEvaluation" "DynamicWorkload.sh" "Dynamic Workload Evaluation"
 
-# Scheduling Strategies Evaluation (Fig.12)
-run_experiment "$project_Dir/scripts/PerformanceEvaluation" "MultipleSchedulingStrategies.sh" "Multiple Scheduling Strategies Evaluation"
+# # Scheduling Strategies Evaluation (Fig.12)
+# run_experiment "$project_Dir/scripts/PerformanceEvaluation" "MultipleSchedulingStrategies.sh" "Multiple Scheduling Strategies Evaluation"
 
-# System Overhead Evaluation (Fig.13)
-run_experiment "$project_Dir/scripts/Overhead" "SystemOverhead.sh" "System Overhead Evaluation"
+# # System Overhead Evaluation (Fig.13)
+# run_experiment "$project_Dir/scripts/Overhead" "SystemOverhead.sh" "System Overhead Evaluation"
 
-# GC Overhead Evaluation (Fig.14)
-run_experiment "$project_Dir/scripts/Overhead" "VaryingJVMSize.sh" "GC Overhead Evaluation"
+# # GC Overhead Evaluation (Fig.14)
+# run_experiment "$project_Dir/scripts/Overhead" "VaryingJVMSize.sh" "GC Overhead Evaluation"
 
-# Scheduling Decisions Evaluation (Fig.15 ~ Fig.17)
-run_experiment "$project_Dir/scripts/SchedulingDecisions" "ExplorationStrategies.sh" "Impact of Scheduling Exploration Strategies"
+# # Scheduling Decisions Evaluation (Fig.15 ~ Fig.17)
+# run_experiment "$project_Dir/scripts/SchedulingDecisions" "ExplorationStrategies.sh" "Impact of Scheduling Exploration Strategies"
 run_experiment "$project_Dir/scripts/SchedulingDecisions" "SchedulingGranularities.sh" "Impact of Scheduling Granularities"
-run_experiment "$project_Dir/scripts/SchedulingDecisions" "AbortHandling.sh" "Impact of Abort Handling Mechanisms"
+# run_experiment "$project_Dir/scripts/SchedulingDecisions" "AbortHandling.sh" "Impact of Abort Handling Mechanisms"
 
-# Modern Hardware Evaluation (Fig.18)
-echo "Running Micro-architectural Analysis on Modern Hardware (Fig.18a)..."
-cd "$project_Dir/scripts/ModernHardware/MicroArchitecturalAnalysis"
-bash vtune_uarch_profiling.sh
-echo "Micro-architectural Analysis completed."
+# # Modern Hardware Evaluation (Fig.18)
+# echo "Running Micro-architectural Analysis on Modern Hardware (Fig.18a)..."
+# cd "$project_Dir/scripts/ModernHardware/MicroArchitecturalAnalysis"
+# bash vtune_uarch_profiling.sh
+# echo "Micro-architectural Analysis completed."
 
-run_experiment "$project_Dir/scripts/ModernHardware" "MulticoreScalability.sh" "Multicore Scalability Evaluation (Fig.18b)"
+# run_experiment "$project_Dir/scripts/ModernHardware" "MulticoreScalability.sh" "Multicore Scalability Evaluation (Fig.18b)"
 
 echo "All experiments completed."
